1f060a6912047ea58bce40424a61f0007473f6cb
dc <dc@localhost.localdomain>
2025-03-29 17:16:20 +0800

    Added colour functions

--- templates/script_format.sh
+++ templates/script_format.sh
@@ -11,6 +11,18 @@ exit_handler() {
 	echo "Quitting..."
 	exit 2 
 }
+black='\033[0;30m'        # Black
+red='\033[0;31m'          # Red
+green='\033[0;32m'        # Green
+yellow='\033[0;33m'       # Yellow
+blue='\033[0;34m'         # Blue
+purple='\033[0;35m'       # Purple
+cyan='\033[0;36m'         # Cyan
+white='\033[0;37m'        # White
+pcol() { 
+	local colour=$2
+	echo -e "$2$1$white"
+}
 # Handle Ctrl^C 
 trap exit_handler SIGINT
 
@@ -27,4 +39,5 @@ done
 # declare -a indexed_arr 
 # indexed_arr=( "v1" "v2" "v3" ) 
 # declare -A assoc_array
-# assoc_arr=(["k1"]="v1" ["k2"]="v2" ["k3"]="v3" ) 
+# assoc_arr=(["k1"]="v1" ["k2"]="v2" ["k3"]="v3" )
+
