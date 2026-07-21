268acc65e085727f7856d7df0cc5fad2a1b9d4b5
Daniel Cheng <daniel.cheng@curtin.edu.au>
2026-07-21 15:08:04 +0800

    got dap setup and fixed cs dap

--- private_dot_config/nvim/lua/plugins/neotree.lua
+++ private_dot_config/nvim/lua/plugins/neotree.lua
@@ -8,7 +8,7 @@ return {
 		"MunifTanjim/nui.nvim",
 		-- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
 	},
-	lazy = false, -- neo-tree will lazily load itself
+	lazy = true,
 	---@module "neo-tree"
 	---@type neotree.Config?
 
--- private_dot_config/nvim/lua/plugins/sessions.lua
+++ private_dot_config/nvim/lua/plugins/sessions.lua
@@ -7,8 +7,25 @@ return {
 	---@module "auto-session"
 	---@type AutoSession.Config
 	opts = {
-		suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-		close_unsupported_windows = false,
+		suppressed_dirs = { "~/Downloads" },
+		close_unsupported_windows = true,
+
+		autosave_ignore_buftypes = {
+			"quickfix",
+			"nofile",
+			"prompt",
+			"neo-tree",
+			"neotree",
+		},
+		autosave_ignore_filetypes = {
+			"dapui_scopes",
+			"dapui_stacks",
+			"dapui_breakpoints",
+			"dapui_watches",
+			"dap-repl",
+			"neo-tree",
+			"neotree",
+		},
 		bypass_session_save_file_types = { "neo-tree", "neotree" },
 	},
 }
